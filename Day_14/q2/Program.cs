6e068fdb152c5bb83e3e5d5bbadf708b528b975e
[Ketan Fulbandhe] <[ketanfulbandhe@gmail.com]>
2020-11-03 22:41:35 +0530

    Day 14 Assignment

---
+++ Day_14/q2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+/*Q2. Write a static class with generic method swap, which should able to swap 2 value of any type and it should also swap two employee object. 
+*/
+
+namespace ConsoleAppl_GenericSwap
+{
+    public static class GenericSwap
+    {
+        public static void MyGenSwap<T>(ref T a, ref T b)
+        {
+           // Console.WriteLine("typeof data: {0}", typeof(T));
+            T temp;
+            temp = a;
+            a = b;
+            b = temp;
+        }
+    }
+    public class Employee
+    {
+        static int getid;
+        int id;
+        string name;
+        int salary;
+        public Employee(string nm, int sal)
+        {
+            id = ++getid;
+            name = nm;
+            salary = sal;
+        }
+        public override string ToString()
+        {
+            Console.WriteLine("id: {0} name: {1} salary: {2}", id, name, salary);
+            return null;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10, b = 20;
+            Console.WriteLine("Before: {0} {1}", a, b);
+            
+            GenericSwap.MyGenSwap<int>(ref a, ref b);
+            Console.WriteLine("After: {0} {1}", a, b);
+
+            Console.WriteLine("\n\n");
+            Console.WriteLine("Before: ");
+            Employee emp1 = new Employee("Tom", 25000);
+            Employee emp2 = new Employee("Todd", 30000);
+            Console.WriteLine("{0} {1}", emp1.ToString(), emp2.ToString());
+            Console.WriteLine("After:");
+            GenericSwap.MyGenSwap<Employee>(ref emp1, ref emp2);
+            Console.WriteLine("{0} {1}", emp1.ToString(), emp2.ToString());
+        }
+    }
+}
---
+++ Day_14/q3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+/*Q4. Write a generic delegate which will point to method which will return square , cube ,and factorial of a number*/
+
+namespace ConsoleApp_GenericDelegate
+{
+    public delegate void MyGenericDelegate<T>(T genD);
+    
+    class Program
+    {
+        public static void square(int s)
+        {
+            Console.WriteLine("Square: " + s * s);
+        }
+        public static void cube(int c)
+        {
+            Console.WriteLine("cube: " + c * c * c);
+        }
+        public static void factorial(int n)
+        {
+            int f=1;
+            for(int i=1;i<=n;i++)
+                f=f*i;
+            Console.WriteLine("factorial: "+f);
+        }
+        static void Main(string[] args)
+        {
+            MyGenericDelegate<int> intSqGenD = new MyGenericDelegate<int>(square);
+            intSqGenD(5);
+
+            MyGenericDelegate<int> intCuGenD = new MyGenericDelegate<int>(cube);
+            intCuGenD(5);
+
+            MyGenericDelegate<int> intFactGenD = new MyGenericDelegate<int>(factorial);
+            intFactGenD(5);
+        }
+    }
+}
