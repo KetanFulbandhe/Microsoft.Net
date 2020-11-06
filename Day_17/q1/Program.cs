64f28235c99c444b4bbb5e02dc6eb4f9cbc968d6
[Ketan Fulbandhe] <[ketanfulbandhe@gmail.com]>
2020-11-06 23:56:38 +0530

    Day 17 Assignment ADO

---
+++ Day_17/q1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Data.SqlClient;
+/*Q1. Create table Emp having column Id, Name, Salary.using ADO.Net display all detail of the table.
+Do not forget to add record in table.*/
+namespace EmployeeADO
+{
+    class Employee
+    {
+        public void EmployeeDetail()
+        {
+            string connectionString = @"Data Source=(localdb)\Projects;Initial Catalog=Demodata;Integrated Security=True";
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("select * from Employee", con);
+                con.Open();
+                Console.WriteLine("Connected!!!");
+                SqlDataReader rdr = cmd.ExecuteReader();
+
+                if (rdr.HasRows)
+                {
+                    while (rdr.Read())
+                    {
+                        Console.WriteLine("{0} {1} {2}", rdr["Id"], rdr["Name"], rdr["Salary"]);
+
+                    }
+                }
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee();
+            emp.EmployeeDetail();
+        }
+    }
+}
---
+++ Day_17/q2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace EmployeeConSetter
+{
+    class Employee
+    {
+        public void EmployeeDetails()
+        {
+            string connectionString = @"Data Source=(localdb)\Projects;Initial Catalog=Demodata;Integrated Security=True";
+
+            using (SqlConnection con = new SqlConnection())
+            {
+                con.ConnectionString = connectionString;
+                SqlCommand cmd = new SqlCommand("Select * from Employee",con);
+                con.Open();
+                Console.WriteLine("Connected!!!");
+                SqlDataReader rdr = cmd.ExecuteReader();
+                if (rdr.HasRows)
+                {
+                    while (rdr.Read())
+                    {
+                        Console.WriteLine("{0} {1} {2}", rdr["Id"], rdr["Name"], rdr["Salary"]);
+                    }
+                }
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee();
+            emp.EmployeeDetails();
+        }
+    }
+}
